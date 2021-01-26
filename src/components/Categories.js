80f068c1dc8aa44e6991e359f9e43850a9313772
Duc Nguyen Minh <duc.nguyen-minh@tu-ilmenau.de>
2021-01-26 17:20:05 +0100

    create tabs with categories from api

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,47 +1,77 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Appbar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 const Categories = (props) => {
-    
-    const [allCategories, setCategories]= useState([]);
-    const [fruits, setFruits]= useState([]);
+  const [allCategories, setCategories] = useState([]);
+  const [fruits, setFruits] = useState([]);
 
-    const getCategories = async () => {
-        const res = await axios.get("https://api.predic8.de/shop/categories/");
-        const categories = res.data.categories;
-        const filtered = await categories.filter(element => { 
-            return element.hasOwnProperty("name") && isNaN(element.name);
-        })
+  const getCategories = async () => {
+    const res = await axios.get("https://api.predic8.de/shop/categories/");
+    const categories = res.data.categories;
+    const filtered = await categories.filter((element) => {
+      return element.hasOwnProperty("name") && isNaN(element.name);
+    });
 
-        const categorieUrl = filtered.map((element) => {
-            return (props.url + element.category_url);
-        })
+    const categorieUrl = filtered.map((element) => {
+      return props.url + element.category_url;
+    });
 
-        const getproductsByCategorie = await categorieUrl.map((url) => {
+    const getproductsByCategorie = await categorieUrl.map((url) => {
+      return axios
+        .get(url)
+        .then((res) => res.data)
+        .catch((err) => console.log(err));
+    });
 
-            return axios
-                .get(url)
-                .then(res => res.data)
-                .catch(err => console.log(err));
-        })
+    axios.all(getproductsByCategorie).then((res) => setCategories(res));
+  };
 
-        axios.all(getproductsByCategorie).then(res => setCategories(res));      
-    }
+  const getFruits = allCategories.map((category) => {
+    return category.products;
+  });
 
-    const getFruits = allCategories.map(category => {
-        return category.products
-    })
-    console.log(allCategories);
-    useEffect(() => {
-        getCategories();
-        
-    }, []);
-    
-    return (
-        <div>
+console.log(getFruits[0]);
 
-        </div>
-    )
-}
+  
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const [tabValue, setTabValue] = useState(0);
+  const handleTabs = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+
+    return <div>{value === index && <p>{children}</p>}</div>;
+  };
+
+  return (
+    <div>
+      <Appbar position="static">
+        <Tabs onChange={handleTabs} value={tabValue}>
+          {allCategories.map((category) => {
+            return <Tab label={category.name}></Tab>;
+          })}
+        </Tabs>
+        <TabPanel value={tabValue} index={0}>
+          Item1
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          Item2
+        </TabPanel>
+        <TabPanel value={tabValue} index={2}>
+          Item3
+        </TabPanel>
+      </Appbar>
+    </div>
+  );
+};
 
 export default Categories;
